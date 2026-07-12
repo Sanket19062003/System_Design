ec44f9f931ecf467841f14de7399b2c30cb0f3c6
Sanket <sanketsutar1906@gmail.com>
2026-07-12 15:47:33 +0530

    Created all the required classes

---
+++ single_responsibility_principle.java
@@ -0,0 +1,21 @@
+class product{
+
+}
+
+class ShoppingCart{
+
+}
+
+class CartInvoicePrinter{
+
+}
+
+class CartDbStorage{
+
+}
+
+class single_responsibility_principle {
+    public static void main(String[] args) {
+
+    }
+}
