b7724ec95b148106222dcb29575629a090c5c04a
Sanket <sanketsutar1906@gmail.com>
2026-07-13 10:38:28 +0530

    Created the structure of the code

---
+++ open_close_principle.java
@@ -0,0 +1,26 @@
+class product{
+
+}
+
+class ShoppingCart{
+
+}
+
+class CartInvoicePrinter{
+
+}
+
+public abstract class DBPersistence{
+
+}
+
+class SaveToSQLDB{
+
+}
+class SaveToMongoDB{
+
+}
+
+class SaveToFile{
+
+}
