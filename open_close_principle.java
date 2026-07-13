f15c9ea028412498feb02d337de24ef21700041d
Sanket <sanketsutar1906@gmail.com>
2026-07-13 10:49:34 +0530

    Created the main class

--- open_close_principle.java
+++ open_close_principle.java
@@ -60,3 +60,26 @@ class SaveToFile implements DBPersistence{
         System.out.println("Save to File");
     }
 }
+
+class open_close_principle{
+    public static void main(String[] args) {
+        ShoppingCart shoppingCart=new ShoppingCart();
+        shoppingCart.addProduct(new product(12000,"Abc"));
+        shoppingCart.addProduct(new product(1000,"xyz"));
+
+        CartInvoicePrinter cartPrinter=new CartInvoicePrinter(shoppingCart);
+        cartPrinter.printInvoice();
+
+        DBPersistence dbPersistence=new SaveToSQLDB();
+        dbPersistence.save();
+
+        DBPersistence dbPersistence1=new SaveToMongoDB();
+        dbPersistence1.save();
+
+        DBPersistence dbPersistence2 = new SaveToFile();
+        dbPersistence2.save();
+
+        System.out.println("Total Price : " + shoppingCart.getTotal());
+
+    }
+}
