e47fb4285e413a1e62dd48f03880cc3de5076f63
Sanket <sanketsutar1906@gmail.com>
2026-07-13 10:41:53 +0530

    Created the content of the class ShoppingCart

--- open_close_principle.java
+++ open_close_principle.java
@@ -8,6 +8,20 @@ class product{
 }
 
 class ShoppingCart{
+    List<product> products=new ArrayList<product>();;
+    public void addProduct(product p){
+        products.add(p);
+    }
+    public List<product> getAllProducts(){
+        return products;
+    }
+    public double getTotal(){
+        double total=0;
+        for(product p:products){
+            total+=p.price;
+        }
+        return total;
+    }
 
 }
 
