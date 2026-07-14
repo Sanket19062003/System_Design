6187b0ae9f83bad1f7dcbb15c21b53b9b9f5e238
Sanket <sanketsutar1906@gmail.com>
2026-07-14 13:23:23 +0530

    Created the nonWithdrawableAccount abstract class

--- liskov_substitution_principle.java
+++ liskov_substitution_principle.java
@@ -13,14 +13,14 @@ class client{
             w.withdraw(500);
         }
         for(nonWithdrawableAccount nw : nwd){
-            nw.deposit(1000.00);
+            nw.deposit(1000.00);g
         }
     }
 
 }
 
 public abstract class nonWithdrawableAccount{
-
+    void deposit(double amount);
 }
 public abstract class withdrawableAccount{
 
