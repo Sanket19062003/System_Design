341707ddc3bcff06801e5518d4f50447dcf2f2ff
Sanket <sanketsutar1906@gmail.com>
2026-07-14 13:18:47 +0530

    Created the Structure of the project

---
+++ liskov_substitution_principle.java
@@ -0,0 +1,19 @@
+class client{
+
+}
+
+public abstract class nonWithdrawableAccount{
+
+}
+public abstract class withdrawableAccount{
+
+}
+
+class fixedDepositAccount{
+}
+
+class currentAccount{
+}
+
+class savingAccount{
+}
