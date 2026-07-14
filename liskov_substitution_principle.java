65c03b7ccf61775eab4de5f704e9816bf683ecbd
Sanket <sanketsutar1906@gmail.com>
2026-07-14 13:22:30 +0530

    Created the client class

--- liskov_substitution_principle.java
+++ liskov_substitution_principle.java
@@ -1,4 +1,21 @@
 class client{
+    List<nonWithdrawableAccount> nwd ;
+    List<wthdrawableAccount> wd;
+
+    client(List<nonWithdrawableAccount> nwd, List<wthdrawableAccount> wd){
+        this.nwd = nwd;
+        this.wd = wd;
+    }
+
+    public void processTransaction(){
+        for(wthdrawableAccount w : wd){
+            w.deposit(1000.00);
+            w.withdraw(500);
+        }
+        for(nonWithdrawableAccount nw : nwd){
+            nw.deposit(1000.00);
+        }
+    }
 
 }
 
